32f1324958aa968502dbfcc9634a9404d98bba8d
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-09-25 20:45:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const form = document.getElementById("form1");
+	const fname = form.elements["fname"].value;
+	const lname = form.elements["lname"].value;
+	alert(fname+" "+lname);
 }
